9654a88942ff78d62dd3dd67460e703978640bf6
parksangseok <park0157@naver.com>
2026-08-28 15:28:30 +0900

    commit

---
+++ js/sub.js
@@ -0,0 +1,16 @@
+$(function () {
+
+  //달력
+  $('.equipmentReservationWrap .dateBtn').click(function () {
+    $('.equipmentReservationWrap .dateBtn').removeClass('active');
+    $(this).addClass('active');
+  });
+
+
+
+  
+
+
+
+
+});
