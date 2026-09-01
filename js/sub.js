ff51fae84427abc32747efc4074755903f08779a
parksangseok <park0157@naver.com>
2026-09-01 17:32:50 +0900

    commit

--- js/sub.js
+++ js/sub.js
@@ -6,9 +6,25 @@ $(function () {
     $(this).addClass('active');
   });
 
-
-
   
+  /* 감염유래시료 여부 */
+  $('input[name="infectiousSample"]').change(function () {
+
+    if ($('#infectiousSampleYes').is(':checked')) {
+      $('#infectiousSampleType')
+        .prop('disabled', false)
+        .prop('required', true)
+        .focus();
+    } else {
+      $('#infectiousSampleType')
+        .prop('disabled', true)
+        .prop('required', false)
+        .val('');
+    }
+
+  });
+
+
 
 
 
